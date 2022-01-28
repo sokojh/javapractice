67f4e57b4f728f503f46501545f51faa7ef30a44
jiho kim <sokojiho2@gmail.com>
2022-01-28 11:15:03 +0900

    switch문예시

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,22 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        //switch문 예시
+        String str = "A";
 
+        switch(str){
+            case "A":
+                System.out.println("1");
+                break;
+            case "B":
+                System.out.println("2");
+                break;
+            case "C" :
+                System.out.println("3");
+                break;
+            default :
+                System.out.println("그 외의 숫자");
+
+        }
     }
 }
---
+++ src/com/company/variableTest.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class variableTest {
+    int Eum = 3;
+
+
+}
