cbfd9f42ce9aaaa80826d9a1587190fd81fad754
jiho kim <sokojiho2@gmail.com>
2022-01-29 21:25:58 +0900

    while문

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,22 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        //switch문 예시
-        String str = "A";
-
-        switch(str){
-            case "A":
-                System.out.println("1");
-                break;
-            case "B":
-                System.out.println("2");
-                break;
-            case "C" :
-                System.out.println("3");
-                break;
-            default :
-                System.out.println("그 외의 숫자");
-
+        //while문 예시
+        int i = 1;
+        int total = 0; // i의 값을 누적할 변수를 선언합니다.int i = 1;
+        while(i <= 100){
+            total = total + i;
+            i++;
         }
+        System.out.println(i);
+
     }
 }
