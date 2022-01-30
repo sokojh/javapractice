46b2981d5f66cbb86f51fadb0f22608de126a956
jiho kim <sokojiho2@gmail.com>
2022-01-30 15:52:01 +0900

    while문응용

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,12 +5,19 @@ public class Main {
     public static void main(String[] args) {
         //while문 예시
         int i = 1;
+        int a = 1;
+        int atotal = 0;
         int total = 0; // i의 값을 누적할 변수를 선언합니다.int i = 1;
         while(i <= 100){
             total = total + i;
             i++;
         }
-        System.out.println(i);
+        while(a<=100){
+            atotal = atotal + a;
+            i++;
+            System.out.println(atotal);
+        }
+
 
     }
 }
