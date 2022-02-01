d086a0f3ab5cf2e8d365b99d11a324b389199c5c
jiho kim <sokojiho2@gmail.com>
2022-02-01 16:33:39 +0900

    class method 실습예제

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,21 +3,10 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        //배열을 for문으로 꺼내는 실습
-        int [] iarray = new int[100];
-        iarray[0] = 1;
-        iarray[1] = 2;
-        System.out.println(iarray.length);
-        for(int i = 0; i < iarray.length;i++){
-            iarray[i] = i + 1;
-            System.out.println(iarray[i]);
-        }
-        //누적해서 더하는 실습
-        int sum = 0;
-        for(int i =0; i <iarray.length;i++){
-            sum = sum+ iarray[i];
-            System.out.println(sum);
-        }
+        //클래스 메소드 실습 예시
+      MyClass cl1 = new MyClass();
+        System.out.println(cl1.Mymethod1(3));
+
 
 
     }
---
+++ src/com/company/MyClass.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class MyClass {
+    public int Mymethod1(int x){
+        System.out.println("Mymethod1실행됨");
+        return x+3;
+
+    }
+}
