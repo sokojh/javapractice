6e27204706251e9cc4c9a69126583a77d06959df
jiho kim <sokojiho2@gmail.com>
2022-02-01 15:20:36 +0900

    배열실습

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,16 +3,21 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        //1차원 배열 실습 예제
-        //int[] array1 = new int[] {1,2,3,4,5};//선언과 초기화,할당을 동시에하는 경우
-       int[] array1 = new  int[4]; //선언 따로 할당따로
-       array1[0] = 1;
-       array1[1] = 2;
-       array1[2] = 3;
-       array1[3] = 4;
-       //array1[4] = 5; 이렇게하면 오류 난다 왜? 우리가 위에서 만든 4라는 숫자는 '크기' 4라는 뜻이다. 배열 4까지가 아니라!
-        System.out.println(array1[1]);
-
+        //배열을 for문으로 꺼내는 실습
+        int [] iarray = new int[100];
+        iarray[0] = 1;
+        iarray[1] = 2;
+        System.out.println(iarray.length);
+        for(int i = 0; i < iarray.length;i++){
+            iarray[i] = i + 1;
+            System.out.println(iarray[i]);
+        }
+        //누적해서 더하는 실습
+        int sum = 0;
+        for(int i =0; i <iarray.length;i++){
+            sum = sum+ iarray[i];
+            System.out.println(sum);
+        }
 
 
     }
